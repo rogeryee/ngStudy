7bc24c34173fb42c6bbc082ae8b59da053032552
rogeryee <roger.yee@hotmail.com>
2014-11-05 16:43:22 +0800

    add more custom directive sample

--- src/app/directiveSample/directiveSample.js
+++ src/app/directiveSample/directiveSample.js
@@ -25,8 +25,12 @@ angular.module('ngStudy.directiveSample', [
       name: 'Naomi',
       address: '1600 Amphitheatre'
     };
+
+    // 3. Link sample
+    $scope.format = 'M/d/yy h:mm:ss a';
   }])
 
+  // 2.Template/TemplateUrl
   .directive('myCustomer', function () {
     return {
       template: 'Use template - Name: {{customer.name}} Address: {{customer.address}}'
@@ -50,4 +54,34 @@ angular.module('ngStudy.directiveSample', [
       }
     };
   })
+
+  // 3. Link sample
+  .directive('myCurrentTime', ['$interval', 'dateFilter', function($interval, dateFilter) {
+    function link(scope, element, attrs) {
+      var format,
+        timeoutId;
+
+      function updateTime() {
+        element.text(dateFilter(new Date(), format));
+      }
+
+      scope.$watch(attrs.myCurrentTime, function(value) {
+        format = value;
+        updateTime();
+      });
+
+      element.on('$destroy', function() {
+        $interval.cancel(timeoutId);
+      });
+
+      // start the UI update process; save the timeoutId for canceling
+      timeoutId = $interval(function() {
+        updateTime(); // update DOM
+      }, 1000);
+    }
+
+    return {
+      link: link
+    };
+  }])
 ;
